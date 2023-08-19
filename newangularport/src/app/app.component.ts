06ebb9550cec8dadc165d87001dcaf836dc625a3
Champ Henry Duco <ducochamp@gmail.com>
2023-08-19 15:44:03 +0800

    code drop

---
+++ newangularport/src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component, ElementRef, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'newangularport';
+  @ViewChild('sidemenu') sidemenu!: ElementRef;
+
+
+  openmenu() {
+    this.sidemenu.nativeElement.style.right = '0';
+  }
+
+  closemenu() {
+    this.sidemenu.nativeElement.style.right = '-200px';
+  }
+}
