c5a833c169726bc91825cbb7c8eb02427ef340de
Champ Henry Duco <ducochamp@gmail.com>
2023-08-19 16:21:21 +0800

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,14 +7,14 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
 })
 export class AppComponent {
   title = 'newangularport';
-  @ViewChild('sidemenu') sidemenu!: ElementRef;
+  // @ViewChild('sidemenu') sidemenu!: ElementRef;
 
 
-  openmenu() {
-    this.sidemenu.nativeElement.style.right = '0';
-  }
+  // openmenu() {
+  //   this.sidemenu.nativeElement.style.right = '0';
+  // }
 
-  closemenu() {
-    this.sidemenu.nativeElement.style.right = '-200px';
-  }
+  // closemenu() {
+  //   this.sidemenu.nativeElement.style.right = '-200px';
+  // }
 }
